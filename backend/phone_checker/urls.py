fe454365357d0433a628971a1d8d8ee2b2d69397
landeryt <mcraft160105@gmail.com>
2025-12-10 19:58:28 +0300

    Fixed the URLs so that it's api/ not /api/api

--- backend/phone_checker/urls.py
+++ backend/phone_checker/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("api/phone/count", views.count, name="count"),
+    path("phone/count", views.count, name="count"),
 
     # API route
-    path("api/phone/validate", views.validate, name="validate"),
-    path("api/registration", views.index, name="registration")
+    path("phone/validate", views.validate, name="validate"),
+    path("registration", views.index, name="registration")
 ]
